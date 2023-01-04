4ef9040bc33ed1aaaa1772cbe3000152865bae6e
FredT93600 <ftetard@asso-auxilia.fr>
2023-01-04 09:25:07 +0100

    message

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+// Je veux afficher un message dans mon inspecteur d'element du navigateur, dans l'onglet console
+// cela me permet de verifier que mon fichier js est bien lié à mon index.html
+
+// je crée une variable.
+let flexMe = "Je dis stop à mon flex";
+// je fais appel à une method native de js pour afficher cette variable dans l'onglet console de l'inspecteur d'élément.
+console.log(flexMe);
